96d7ec421184323d6c852008cbf6867c4cb2debe
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-06-08 12:59:50 +0530

    :bug: Fixed image quality

--- api/articles.ts
+++ api/articles.ts
@@ -1,30 +1,44 @@
-import { NowRequest, NowResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { addHeaders } from "../utils/header";
 import { getArticlesEndpoint } from "../utils/endpoint";
 import xray from "x-ray";
 
 const x = xray();
 
-module.exports = (req: NowRequest, res: NowResponse) => {
+module.exports = (req: VercelRequest, res: VercelResponse) => {
   addHeaders(res);
   const tag = req.query.tag as string;
 
-  x(getArticlesEndpoint(tag), ".post-block", [
-    {
-      title: ".post-block__header h2",
-      link: ".post-block__title a@href",
-      timestamp: "time@datetime",
-      author: x(".river-byline__authors a", ["@html"]),
-      description: ".post-block__content @text",
-      image: ".post-block__media img@src",
-    },
-  ])((err, articles) => {
-    if (err) res.json(err);
-    var responseJson = new Object();
-    var str = JSON.stringify(articles);
-    var convertedStr = str.replace(/(\\n)*(\\t)*/g, "");
-    responseJson["total"] = Object.keys(articles).length;
-    responseJson["articles"] = JSON.parse(convertedStr);
-    res.json(responseJson);
-  });
+  try {
+    x(getArticlesEndpoint(tag), ".post-block", [
+      {
+        title: ".post-block__header h2",
+        link: ".post-block__title a@href",
+        timestamp: "time@datetime",
+        author: x(".river-byline__authors a", ["@html"]),
+        description: ".post-block__content @text",
+        image: ".post-block__media img@src",
+      },
+    ])((error, articles) => {
+      let responseJson = new Object();
+      let articlesArray = new Array();
+      let str = JSON.stringify(articles).replace(/(\\n)*(\\t)*/g, "");
+
+      JSON.parse(str).forEach((article: any) => {
+        article.image = sanitizeImage(article.image);
+        articlesArray.push(article);
+      });
+
+      responseJson["total"] = Object.keys(articles).length;
+      responseJson["articles"] = articlesArray;
+
+      res.json(responseJson);
+    });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+const sanitizeImage = (imageLink: string) => {
+  return imageLink.split("?")[0];
 };
